4e8207a5024a0e290c25b10b7ae0e69aac992e81
João Rafael Pacheco <jrpacheco.dev@gmail.com>
2021-03-19 11:02:33 +0000

    ?

--- Bot.ts
+++ Bot.ts
@@ -1,42 +1,60 @@
-import {Client, Collection, Intents} from "discord.js";
+import { Client, Collection, Intents, WebhookClient } from "discord.js";
 import { readdirSync } from "fs";
 import { ICommand } from "./interfaces/ICommands";
 import { IEvent } from "./interfaces/IEvents";
+import cron from "cron";
 
-class Bot extends Client{
-    public prefix : string;
-    public commands : Collection<string, ICommand> = new Collection();
-    public event : Collection<string, IEvent> = new Collection();
+class Bot extends Client {
+  public prefix: string;
+  public commands: Collection<string, ICommand> = new Collection();
+  public event: Collection<string, IEvent> = new Collection();
 
-    public constructor() {
-        super();
-    }
+  public constructor() {
+    super();
+  }
 
-    public init = () => {
-        this.prefix = process.env.DISCORD_PREFIX;
-        
-        this.login(process.env.DISCORD_TOKEN);
+  public init = () => {
+    this.prefix = process.env.DISCORD_PREFIX;
 
-        const command_files = readdirSync("./commands/").filter((file) => file.endsWith(".ts"));
-        command_files.map(async (fileName:string) => {
-            const command = (await import(`./commands/${fileName}`)) as ICommand;
-            this.commands.set(command.name, command);
-        });
+    this.login(process.env.DISCORD_TOKEN);
 
-        let dirs = ["client", "server"];
+    const command_files = readdirSync("./commands/").filter((file) =>
+      file.endsWith(".ts")
+    );
+    command_files.map(async (fileName: string) => {
+      const command = (await import(`./commands/${fileName}`)) as ICommand;
+      this.commands.set(command.name, command);
+    });
 
-        var event_files;
-        const load_dir = async (dirs) => {
-            event_files = readdirSync(`./events/${dirs}`).filter((file) => file.endsWith(".ts"));
+    let dirs = ["client", "server"];
 
-            event_files.map(async (filename: string) =>{
-                const event = (await import(`./events/${dirs}/${filename}`)) as IEvent;
+    var event_files;
+    const load_dir = async (dirs) => {
+      event_files = readdirSync(`./events/${dirs}`).filter((file) =>
+        file.endsWith(".ts")
+      );
 
-                const event_name = event.name;
-                this.on(event_name, event.execute.bind(null, this));
-            })
-        }
-        ["client", "server"].forEach((e) => load_dir(e));
-    }
+      event_files.map(async (filename: string) => {
+        const event = (await import(`./events/${dirs}/${filename}`)) as IEvent;
+
+        const event_name = event.name;
+        this.on(event_name, event.execute.bind(null, this));
+      });
+    };
+    ["client", "server"].forEach((e) => load_dir(e));
+
+    const mufasa = new WebhookClient(
+      "mufasa",
+      "https://canary.discord.com/api/webhooks/693295402555801611/GFZ0XiChiDX5aIcT8oeWFioguAJQghviDAo5e8lrjvJPfcwdLAdGKvtx6iKQ_CSH2l-W"
+    );
+
+    let scheduledMessage = new cron.CronJob("00 30 11 * * 5", () => {
+      // This runs every day at 10:30:00, you can do anything you want
+
+      mufasa.send("It's friday! https://www.youtube.com/watch?v=1AnG04qnLqI");
+    });
+
+    scheduledMessage.start();
+  };
 }
-export {Bot};
+export { Bot };
--- commands/pong.ts
+++ commands/pong.ts
@@ -2,6 +2,6 @@ import { IExecute } from "../interfaces/ICommands";
 
 export const name = "pong";
 export const description = "pong ping";
-export const execute:IExecute = async (client, message, args) => {
-    message.channel.send('ping!');
-}
+export const execute: IExecute = async (client, message, args) => {
+  message.channel.send("ping!");
+};
