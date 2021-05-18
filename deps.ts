8c081c9578951590ff54074a0a4de05c659aae7c
João Rafael Pacheco <jrpacheco.dev@gmail.com>
2021-05-18 19:30:36 +0100

    stuff

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
-export * from "https://deno.land/std@0.95.0/fmt/colors.ts";
-export * from "https://deno.land/x/sabr@1.1.4/mod.ts";
+export * from "https://deno.land/std/fmt/colors.ts";
+export * from "https://deno.land/x/sabr/mod.ts";
 export * from "https://raw.githubusercontent.com/discordeno/discordeno/main/mod.ts";
 
 export {
