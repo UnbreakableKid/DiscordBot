ef391f7a93decab3d798cc7dde47f0d3a4a4a22b
João Rafael Pacheco <jrpacheco.dev@gmail.com>
2021-05-20 17:15:05 +0100

    stoopiod

--- src/commands/voice/play.ts
+++ src/commands/voice/play.ts
@@ -11,93 +11,68 @@ createCommand({
   name: "play",
   aliases: ["p"],
   guildOnly: true,
-  slash: {
-    enabled: true,
-    guild: true, 
-    options: [
-      {
-        required: true,
-        name: "commands/play:USER_NAME",
-        description: "commands/play:USER_DESCRIPTION",
-        type: DiscordApplicationCommandOptionTypes.String,
-      },
-    ],
-    execute: async (data, member) => {
+  // slash: {
+  //   enabled: true,
+  //   guild: true, 
+  //   options: [
+  //     {
+  //       required: true,
+  //       name: "commands/play:USER_NAME",
+  //       description: "commands/play:USER_DESCRIPTION",
+  //       type: DiscordApplicationCommandOptionTypes.String,
+  //     },
+  //   ],
+  //   execute: (data, member) => {
     
-      var payload;
+  //     var payload;
 
-      const arg = data.data?.options?.[0];
+  //     const arg = data.data?.options?.[0];
       
-      if(arg?.type !== DiscordApplicationCommandOptionTypes.String)
-        return;
+  //     if(arg?.type !== DiscordApplicationCommandOptionTypes.String)
+  //       return;
 
-      const player = bot.lavadenoManager.players.get(
-        data.guildId!.toString(),
-        );
+  //     const player = bot.lavadenoManager.players.get(
+  //       data.guildId!.toString(),
+  //       );
         
-        var toSend: DiscordenoInteractionResponse = {
-          data: { content: payload },
-          type: 4,
-        };
+  //       var toSend: DiscordenoInteractionResponse = {
+  //         data: { content: payload },
+  //         type: 4,
+  //       };
     
-      if (!player || !player.connected) {
+  //     if (!player || !player.connected) {
 
-        const guild = cache.guilds.get(BigInt(data.guildId));
-        const voiceState = guild!.voiceStates.get(BigInt(member?.id));
-        if (!voiceState?.channelId) {
-          payload = (`You first need to join a voice channel!`);
-        }
-        else{
+  //       const guild = cache.guilds.get(BigInt(data.guildId));
+  //       const voiceState = guild!.voiceStates.get(BigInt(member?.id));
+  //       if (!voiceState?.channelId) {
+  //         payload = (`You first need to join a voice channel!`);
+  //       }
+  //       else{
   
-        if (player) {
-          player.connect(voiceState.channelId.toString(), {
-            selfDeaf: true,
-          });
-        } else {
-          const newPlayer = bot.lavadenoManager.create(
-            data.guildId!.toString(),
-          );
-          newPlayer.connect(voiceState.channelId.toString(), {
-            selfDeaf: true,
-          });
-        }
+  //       if (player) {
+  //         player.connect(voiceState.channelId.toString(), {
+  //           selfDeaf: true,
+  //         });
+  //       } else {
+  //         const newPlayer = bot.lavadenoManager.create(
+  //           data.guildId!.toString(),
+  //         );
+  //         newPlayer.connect(voiceState.channelId.toString(), {
+  //           selfDeaf: true,
+  //         });
+  //       }
   
-        payload = (`Successfully joined the channel!`);
-      }
+  //       payload = (`Successfully joined the channel!`);
+  //     }
   
-      const trackSearch = validURL(arg.value)
-        ? arg.value
-        : `ytsearch:${arg.value}`;
-      const result = await bot.lavadenoManager.search(trackSearch);
-
-  
-      // switch (result.loadType) {
-      //   case "TRACK_LOADED":
-      //   case "SEARCH_RESULT": {
-      //     addSoundToQueue(bla, result.tracks[0]);
-      //     break;
-      //   }
-      //   case "PLAYLIST_LOADED": {
-      //     addPlaylistToQueue(
-      //       bla,
-      //       result.playlistInfo!.name,
-      //       result.tracks,
-      //     );
-      //     break;
-      //   }
-      //   default:
-      //     payload = (`Could not find any song with that name!`);
-      // }
-
-
-      return sendInteractionResponse(
-        snowflakeToBigint(data.id),
-        data.token,
-        toSend,
-        );
-      }
-    },
-  },
+  //     return sendInteractionResponse(
+  //       snowflakeToBigint(data.id),
+  //       data.token,
+  //       toSend,
+  //       );
+  //     }
+  //   },
+  // },
   arguments: [{ type: "...strings", name: "query", required: true }],
   async execute(message, args) {
     const player = bot.lavadenoManager.players.get(
