b9e3dee9686d9fe58a2a9e3399bf1a97361de9bf
João Rafael Pacheco <jrpacheco.dev@gmail.com>
2021-05-20 12:45:32 +0100

    save

--- deps.ts
+++ deps.ts
@@ -7,3 +7,12 @@ export {
   Player,
 } from "https://raw.githubusercontent.com/discordeno/lavadeno/master/mod.ts";
 export type { Track } from "https://raw.githubusercontent.com/discordeno/lavadeno/master/mod.ts";
+
+export {
+  json,
+  serve,
+  validateRequest,
+} from "https://deno.land/x/sift/mod.ts";
+
+export * from "https://raw.githubusercontent.com/discordeno/discordeno/main/src/interactions/mod.ts";
+export { decode } from "https://deno.land/std/encoding/base64url.ts"
--- mod.ts
+++ mod.ts
@@ -1,12 +1,21 @@
 import { bot } from "./cache.ts";
 import { configs } from "./configs.ts";
-import { startBot } from "./deps.ts";
+import { camelize,
+  DiscordInteractionResponseTypes,
+  Interaction,
+  InteractionResponseTypes,
+  InteractionTypes,
+  json,
+  serve,
+  validateRequest,
+  verifySignature, startBot } from "./deps.ts";
 import { fileLoader, importDirectory } from "./src/utils/helpers.ts";
-import { loadLanguages } from "./src/utils/i18next.ts";
+import { loadLanguages, translate } from "./src/utils/i18next.ts";
 import { log } from "./src/utils/logger.ts";
 import { config } from "https://deno.land/x/dotenv/mod.ts";
 import "https://deno.land/x/dotenv/load.ts";
 import keepAlive from "./server.ts";
+
 
 log.info(
   "Beginning Bot Startup Process. This can take a little bit depending on your system. Loading now...",
@@ -31,6 +40,7 @@ await fileLoader();
 await loadLanguages();
 await import("./src/database/database.ts");
 
+
 startBot({
   token: Deno.env.get("TOKEN")!,
   // Pick the intents you wish to have for your bot.
@@ -41,6 +51,101 @@ startBot({
 });
 
 keepAlive();
+
+serve({
+  "/": main,
+});
+
+async function main(request: Request) {
+  // Validate the incmoing request; whether or not, it includes
+  // the specified headers that are sent by Discord.
+  const { error } = await validateRequest(request, {
+    POST: {
+      headers: ["X-Signature-Ed25519", "X-Signature-Timestamp"],
+    },
+  });
+  if (error) {
+    return json({ error: error.message }, { status: error.status });
+  }
+
+  const publicKey = Deno.env.get("DISCORD_PUBLIC_KEY");
+  if (!publicKey) {
+    return json({
+      error: "Missing Discord public key from environment variables.",
+    });
+  }
+
+  const signature = request.headers.get("X-Signature-Ed25519")!;
+  const timestamp = request.headers.get("X-Signature-Timestamp")!;
+
+  const { body, isValid } = verifySignature({
+    publicKey,
+    signature,
+    timestamp,
+    body: await request.text(),
+  });
+  if (!isValid) {
+    return json({ error: "Invalid request; could not verify the request" }, {
+      status: 401,
+    });
+  }
+
+  const payload = camelize<Interaction>(JSON.parse(body));
+  if (payload.type === InteractionTypes.Ping) {
+    return json({
+      type: InteractionResponseTypes.Pong,
+    });
+  } else if (payload.type === InteractionTypes.ApplicationCommand) {
+    if (!payload.data?.name) {
+      return json({
+        type: InteractionResponseTypes.ChannelMessageWithSource,
+        data: {
+          content:
+            "Something went wrong. I was not able to find the command name in the payload sent by Discord.",
+        },
+      });
+    }
+
+    const command = bot.commands.get(payload.data.name);
+    if (!command) {
+      return json({
+        type: InteractionResponseTypes.ChannelMessageWithSource,
+        data: {
+          content: "Something went wrong. I was not able to find this command.",
+        },
+      });
+    }
+
+    // Make sure the user has the permission to run this command.
+    // if (!(await hasPermissionLevel(command, payload))) {
+      // return json({
+      //   type: InteractionResponseTypes.ChannelMessageWithSource,
+      //   data: {
+      //     content: translate(
+      //       payload.guildId!,
+      //       "MISSING_PERM_LEVEL",
+      //     ),
+      //   },
+      // });
+    // }
+
+    if(typeof command !== 'undefined' ){
+    const result = await command.execute(payload);
+    // if (!isInteractionResponse(result)) {
+    //   await logWebhook(payload).catch(console.error);
+    //   return json({
+    //     data: result,
+    //     type: DiscordInteractionResponseTypes.ChannelMessageWithSource,
+    //   });
+    // }
+
+    // DENO/TS BUG DOESNT LET US SEND A OBJECT WITHOUT THIS OVERRIDE
+    return json(result as unknown as { [key: string]: unknown });
+  }
+}
+
+  return json({ error: "Bad request" }, { status: 400 });
+}
 
 // const interaction = new DiscordInteractions({
 //   applicationId: "843485383224197140",
