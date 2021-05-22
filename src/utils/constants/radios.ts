7d8cc40f00cb0e4da8ce1b58762920755af42247
UnbreakableKid <UnbreakableKid@users.noreply.github.com>
2021-05-22 16:42:49 +0000

    Commit from GitHub Actions (Lint)

--- src/utils/constants/radios.ts
+++ src/utils/constants/radios.ts
@@ -1,28 +1,35 @@
 export const Radios = {
   ANTENA_3: {
     value: "antena3",
-    name: "http://radiocast.rtp.pt/antena380a.mp3"
+    name: "http://radiocast.rtp.pt/antena380a.mp3",
   },
   BOB: {
     value: "radiobob",
-    name:"BOB"
+    name: "BOB",
   },
   BOB_BEST_OF_ROCK: {
     value: "radiobobbestofrock",
-    name: "BOB ROCK"
+    name: "BOB ROCK",
   },
   BOB_CLASSIC_ROCK: {
     value: "radiobobclassicrock",
-    name: "BOB CLASSIC ROCK"
+    name: "BOB CLASSIC ROCK",
   },
   BOB_METAL: {
     value: "radiobobmetal",
-    name: "BOB METAL"
+    name: "BOB METAL",
   },
   BOB_ALTERNATIVE: {
     value: "radiobobalternative",
-    name: "BOB ALTERNATIVE"
-  }
-}
+    name: "BOB ALTERNATIVE",
+  },
+};
 
-export const Try = [Radios.ANTENA_3, Radios.BOB, Radios.BOB_ALTERNATIVE, Radios.BOB_BEST_OF_ROCK, Radios.BOB_CLASSIC_ROCK, Radios.BOB_METAL]
+export const Try = [
+  Radios.ANTENA_3,
+  Radios.BOB,
+  Radios.BOB_ALTERNATIVE,
+  Radios.BOB_BEST_OF_ROCK,
+  Radios.BOB_CLASSIC_ROCK,
+  Radios.BOB_METAL,
+];
